416d7ff6f48eabb164cbe610e8e135c0b0c63019
Forixaim <thebest20067@gmail.com>
2022-05-30 16:52:07 -0700

    added items and registers

---
+++ src/main/java/com/forixaim/omneria/block/ForiBlocks.java
@@ -0,0 +1,32 @@
+package com.forixaim.omneria.block;
+
+import com.forixaim.omneria.Omneria;
+import com.forixaim.omneria.items.ForiItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import java.util.function.Supplier;
+
+public class ForiBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Omneria.ModID);
+
+	public static final RegistryObject<Block> Orichalcum_Block = RegisterBlock("orichalcum_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(9f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MATERIALS);
+
+	public static <T extends Block> RegistryObject<T> RegisterBlock(String Name, Supplier<T> Block, CreativeModeTab Tab)
+	{
+		RegistryObject<T> toReturn = BLOCKS.register(Name, Block);
+		RegisterBlockItem(Name, toReturn, Tab);
+		return toReturn;
+	}
+
+    private static <T extends Block> RegistryObject<Item> RegisterBlockItem(String Name, RegistryObject<T> Block, CreativeModeTab Tab)
+    {
+        return ForiItems.ITEMS.register(Name, () -> new BlockItem(Block.get(), new Item.Properties().tab(Tab)));
+    }
+}
--- src/main/java/com/forixaim/omneria/items/ForiItems.java
+++ src/main/java/com/forixaim/omneria/items/ForiItems.java
@@ -8,10 +8,13 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-public class ForiItems {
+public class ForiItems
+{
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Omneria.ModID);
     public static final RegistryObject<Item> Raw_Orichalcum = ITEMS.register("raw_orichalcum", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
     public static final RegistryObject<Item> Orichalcum_Ingot = ITEMS.register("orichalcum_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+    public static final RegistryObject<Item> Orichalcum_Dust = ITEMS.register("orichalcum_dust", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+    public static final RegistryObject<Item> Orichalcum_Nugget = ITEMS.register("orichalcum_nugget", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
     public static void Register(IEventBus Bus)
     {
         ITEMS.register(Bus);
